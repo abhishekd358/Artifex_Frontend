4fb3c7777034f1237b0e4891ac87b1e8e148752f
Abhishek D <abhidhaware1234@gmail.com>
2025-07-27 17:35:52 +0530

    fix name fix name convection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import HeroSection from '../components/HeroSection'
 import WorkSection from '../components/WorkSection'
 import CreateImageSection from '../components/CreateImageSection'
 import TestimonalSection from '../components/TestimonalSection'
-import BottomSection from '../components/bottomSection'
+import BottomSection from '../components/BottomSection'
 
 const Home = () => {
   return (
